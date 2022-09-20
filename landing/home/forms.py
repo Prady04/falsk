499025cb897c0879cdfb7fc79632b2062a3bd974
prady04 <pradyhangsout@gmail.com>
2022-09-20 06:33:43 +0530

    performance optimization

--- landing/home/forms.py
+++ landing/home/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, ValidationError, HiddenField
 
+from sqlalchemy import not_
+
 from .models import EmailSignup
 
 class LandingForm(FlaskForm):
@@ -22,11 +24,12 @@ class LandingForm(FlaskForm):
         ])
 
     def validate_email(self, field):
-        _id = self.data.get('id')
+        _id = self.data.get('id',-1)
         if field.data.endswith(".edu"):
             raise ValidationError('You cannot use a school email address.')
+        not_query = not_(EmailSignup.id==_id)
         if _id is None:
-            obj = EmailSignup.query.filter_by(email=field.data).first()
+            obj = EmailSignup.query.filter_by(email=field.data).filter(not_query).first()
             if obj is not None:
                 msg = 'This email already exists'
                 raise ValidationError(msg)
