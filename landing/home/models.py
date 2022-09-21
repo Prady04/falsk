7f75ec1cd8d1cba918142efd902abb0024d58036
prady04 <pradyhangsout@gmail.com>
2022-09-21 16:24:23 +0530

    db events skeleton

--- landing/home/models.py
+++ landing/home/models.py
@@ -1,4 +1,5 @@
 from landing import db
+from sqlalchemy import event
 
 class EmailSignup(db.Model):
   id = db.Column(db.Integer, primary_key = True)
@@ -32,6 +33,32 @@ class EmailSignup(db.Model):
       return True
     return False 
   
+  
+  # https://docs.sqlalchemy.org/en/latest/orm/events.html
+
+
+# before_insert 
+# after_insert
+
+# before_update
+# after_update
+
+# before_delete
+# after_delete
+
+@event.listens_for(EmailSignup, 'before_update')
+def email_signup_pre_update_signal(mapper, connection, target):
+    pass
+    # target = instance
+    # target.full_name = target.full_name + " working..."
+    # if target.slug is None:
+    #     target.slug = full_name.lower().replace(" ", "-")
+
+@event.listens_for(EmailSignup, 'after_update')
+def email_signup_post_update_signal(mapper, connection, target):
+    # target = instance
+    assert target.id is not None
+
     
       
       
